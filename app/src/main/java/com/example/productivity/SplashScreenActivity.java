3cd64e9411895b61bfc3361b65d3c5229d4203b7
mukarramion <mukarramion@gmail.com>
2023-06-08 06:29:36 +0530

    added splashscreen

---
+++ app/src/main/java/com/example/productivity/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.productivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static final long SPLASH_DELAY = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
